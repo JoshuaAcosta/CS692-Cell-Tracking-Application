9e4f5ee0ef1cfedec8fd97bee002ffc4ec935ddd
Joshua Acosta <Acosta.joshua03@gmail.com>
2017-03-26 21:59:17 -0400

    renamed variables

--- celltracker.py
+++ celltracker.py
@@ -19,19 +19,19 @@ third_frame = Frame(root)
 third_frame.grid(row=0, column=3, rowspan=9, columnspan=3, sticky=W+E+N+S)
 
 
-#Images
+# Images
 img = Image.open("Frame001.png")
 photo = ImageTk.PhotoImage(img)
 pic = Label(third_frame, image=photo)
 
 # Buttons
-a = Button(first_frame, text="Open")
-b = Button(first_frame, text="Save")
-c = Button(first_frame, text="Start")
-d = Button(first_frame, text="Track")
-e = Button(first_frame, text="Pause")
-f = Button(first_frame, text="Track One Frame")
-g = Button(first_frame, text="Divide")
+open_btn = Button(first_frame, text="Open")
+save_btn = Button(first_frame, text="Save")
+start_btn = Button(first_frame, text="Start")
+track_btn = Button(first_frame, text="Track")
+pause_btn = Button(first_frame, text="Pause")
+trk_one_btn = Button(first_frame, text="Track One Frame")
+divide_btn = Button(first_frame, text="Divide")
 
 # Frame Labels
 number_label = Label(first_frame, text="N. of Frames:")
@@ -42,34 +42,34 @@ current_frame_num = Label(first_frame, text="1")
 
 # Compaction
 compaction_label = Label(first_frame, text="Compaction")
-h = Button(first_frame, text="Start")
-i = Button(first_frame, text="End")
+comp_start_btn = Button(first_frame, text="Start")
+comp_end_btn = Button(first_frame, text="End")
 
 # Blastocyst Caviation
 blastocyst_label = Label(first_frame, text="Blastocyst Cavitation")
-j = Button(first_frame, text="Start")
+bc_start_btn = Button(first_frame, text="Start")
 
 # Text box
 instruction = "Instructional text \n will display in this text box. \n"
-T = Label(second_frame, text=instruction, relief=RIDGE, height=15, width=32)
+inst = Label(second_frame, text=instruction, relief=RIDGE, height=15, width=32)
 
-a.grid(row=0, column=0, columnspan=2)
-b.grid(row=0, column=1, columnspan=2)
+open_btn.grid(row=0, column=0, columnspan=2)
+save_btn.grid(row=0, column=1, columnspan=2)
 number_label.grid(row=1, column=0)
 current_label.grid(row=2, column=0)
 number.grid(row=1, column=1)
 current_frame_num.grid(row=2, column=1)
-c.grid(row=3, column=0, columnspan=2)
-d.grid(row=4, column=0, columnspan=2)
-e.grid(row=4, column=1, columnspan=2)
-f.grid(row=5, column=0, columnspan=3)
-g.grid(row=6, column=0, columnspan=3)
+start_btn.grid(row=3, column=0, columnspan=2)
+track_btn.grid(row=4, column=0, columnspan=2)
+pause_btn.grid(row=4, column=1, columnspan=2)
+trk_one_btn.grid(row=5, column=0, columnspan=3)
+divide_btn.grid(row=6, column=0, columnspan=3)
 compaction_label.grid(row=7, column=0, columnspan=2)
-h.grid(row=8, column=0)
-i.grid(row=8, column=1)
+comp_start_btn.grid(row=8, column=0)
+comp_end_btn.grid(row=8, column=1)
 blastocyst_label.grid(row=7, column=2)
-j.grid(row=8, column=2)
-T.grid()
+bc_start_btn.grid(row=8, column=2)
+inst.grid()
 pic.grid()
 
 root.mainloop()
