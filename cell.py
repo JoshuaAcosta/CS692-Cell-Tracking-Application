71f5078151a3c828881a32d0ef51dbcaa98cd844
Joshua Acosta <Acosta.joshua03@gmail.com>
2017-04-09 16:18:03 -0400

    create cell class and point structure as a class

--- cell.py
+++ cell.py
@@ -1,60 +1,17 @@
+class Point ():
+    def __init__(self):
+        self.row = 0
+        self.col = 0
 
 
-struct point {
-    int row;
-    int col;
-};
-typedef struct point point;
-
-@interface Cell : NSObject {
-    BOOL active;
-    BOOL tracked;
-    int generation;
-    int centerRow;
-    int centerCol;
-    int radius;
-    int nLocations;
-    int bandLength;
-    point * boundary;
-}
-
-- (id)initWithNLocations:(int)theNLocations bandLength:(int)theBandLength;
-
-@property(readwrite) BOOL active;
-@property(readwrite) BOOL tracked;
-@property(readwrite) int generation;
-@property(readwrite) int centerRow;
-@property(readwrite) int centerCol;
-@property(readwrite) int radius;
-@property(readwrite) int nLocations;
-@property(readwrite, assign) point * boundary;
-
-
-
-@synthesize active, tracked, generation, centerRow, centerCol, radius, nLocations, boundary;
-
-- (id)initWithNLocations:(int)theNLocations bandLength:(int)theBandLength
-{
-    if (self = [super init]) {
-        active = NO;
-        centerRow = 0;
-        centerCol = 0;
-        radius = 0;
-        nLocations = theNLocations;
-        bandLength = theBandLength;
-        boundary = (point *)malloc(nLocations*sizeof(point));
-    }
-    return self;
-}
-
-
-class Cell (self):
+class Cell ():
     def __init__(self, theNLocations, theBandLength):
         self.theNLocations = theNLocations
-        self.stheBandLength = stheBandLength
-        active = False
-        tracked = False
-        generation = 0
-        centerRow = 0
-        centerCol = 0
-        radius = 0
+        self.theBandLength = theBandLength
+        self.active = False
+        self.tracked = False
+        self.generation = 0
+        self.centerRow = 0
+        self.centerCol = 0
+        self.radius = 0
+        self.boundary = Point()
